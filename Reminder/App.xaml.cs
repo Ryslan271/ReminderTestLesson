0189a4270e6fa58318e264746b7bf9c0115b71c8
232005 <232005@WINMAC18>
2023-01-26 09:28:12 +0300

    Добавьте файлы проекта.

---
+++ Reminder/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Reminder.CocnectDB;
+using System.Data.Entity;
+using System.Windows;
+
+namespace Reminder
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ReminderDBEntities DBConnect = new ReminderDBEntities();
+
+        public App()
+        {
+            DBConnect.User.Load();
+            DBConnect.ReminderTab.Load();
+        }
+    }
+}
---
+++ Reminder/Windows/Login.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Reminder.CocnectDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Reminder.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateData())
+                return;
+
+            User user = App.DBConnect.User.FirstOrDefault(x => x.Login == LoginTextBox.Text.Trim() && x.Password == PasswordTextBox.Text.Trim());
+
+            if (user == null)
+            {
+                MessageBox.Show("Такого пользователя нет в системе");
+                return;
+            }
+
+            new MainWindow().ShowDialog();
+            Hide();
+        }
+
+        private bool ValidateData() =>
+            LoginTextBox.Text == "" &&
+            PasswordTextBox.Text == "";
+
+        private void RegistButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
